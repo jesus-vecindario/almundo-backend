efef988cd53a0c3185a538a7c9ca6c23c29ea1cd
Jesus Martinez <jesusda91@gmail.com>
2018-04-28 14:20:42 -0500

    add error handler

--- app.js
+++ app.js
@@ -12,4 +12,13 @@ app.use(bodyParser.json());
 app.use('/api/', router);
 app.use('/api/hotels', routerHotels);
 
+
+app.use((err, req, res, next) => {
+    if (err.message.match(/not found/)) {
+        return res.status(404).send({ error: err.message })
+    }
+
+    res.status(500).send({ error: err.message })
+})
+
 module.exports = app;
