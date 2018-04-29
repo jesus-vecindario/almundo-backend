001253713ab1117f7ea08d7f64dd0fc9dd54a911
Jesus Martinez <jesusda91@gmail.com>
2018-04-29 12:41:15 -0500

    remove dotenv

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const config = require('./config');
 mongoose.connect(config.db, (err, res) => {
     if (err) throw err
     console.log("Connection established")
-    app.listen(3000, () => {
-        
+    app.listen(config.port, () => {
+        console.log(`API REST ${config.port}`)
     });
 });
