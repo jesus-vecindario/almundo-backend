7d2ae0ff5868370a86c9b431f13ac11203ed051e
Jesus Martinez <jesusda91@gmail.com>
2018-04-29 12:33:30 -0500

    change key mongodb to mongodb_uri

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ require('dotenv').load();
 
 module.exports = {
     port: process.env.PORT || 3000,
-    db: process.env.MONGODB || 'mongodb://localhost:27017/almundo'
+    db: process.env.MONGODB_URI || 'mongodb://localhost:27017/almundo'
 }
