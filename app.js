eacdea4f9eb46205a840d118265282d6d9ed839a
Jesus Martinez <jesusda91@gmail.com>
2018-07-16 17:42:50 -0500

    add view with handlebars

--- app.js
+++ app.js
@@ -3,14 +3,27 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const hbs = require('express-handlebars');
 
 const router = require('./routes/index');
 const routerHotels = require('./routes/hotels');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+app.engine('.hbs', hbs({
+    defaultLayout: 'default',
+    extname: ".hbs"
+}));
+
+app.set('view engine', '.hbs');
+
 app.use('/api/', router);
 app.use('/api/hotels', routerHotels);
+
+app.get('/login', (req, res) => {
+    res.render("login")
+});
 
 app.use((err, req, res, next) => {
     if (err.message.match(/not found/)) {
--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    port: process.env.PORT || 3000,
+    port: process.env.PORT || 3002,
     db: process.env.MONGODB_URI || 'mongodb://localhost:27017/almundo'
 }
